243dfc5886f1daa9f74a5fd317012d052727b168
Syed Wali Uddin Quadri <waliwasser29@gmail.com>
2024-08-19 16:45:10 -0500

    Updated the Linked List by adding new methods: search and reverse

--- LinkedList.py
+++ LinkedList.py
@@ -44,6 +44,31 @@ class LinkedList:
         temp = None
         prev.next = None
 
+    def search(self, key):
+        if self.head is None:
+            print("List is empty")
+            return
+        temp = self.head
+        while temp:
+            if temp.data == key:
+                print("Key found:", temp.data)
+                return
+            temp = temp.next
+        print("Key not found in the list")
+
+    def reverse(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        prev = None
+        current = self.head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+
 
 ll = LinkedList()
 ll.add(5)
@@ -55,5 +80,11 @@ ll.add(6)
 ll.printLL()
 
 ll.remove()
+
+ll.printLL()
+
+ll.search(5)
+
+ll.reverse()
 
 ll.printLL()
